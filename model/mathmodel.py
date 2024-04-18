d3f5180dc2d41994ff987aca8f94350836f25c39
adria.dieguez <adria.dieguez@estudiantat.upc.edu>
2024-04-18 15:24:10 +0200

    Dirichlet parameter updates

--- model/mathmodel.py
+++ model/mathmodel.py
@@ -24,107 +24,150 @@ def generate_alignment(length, distribution):
     """
     Generates an alignment of length `length` using a given `distribution`.
     """
-    seq = ''
-    for i in range(length):
-        # Generate a random sample from the multinomial distribution
-        nucleotide = np.random.choice(['A', 'G', 'C', 'T'], p=distribution)
-        seq += nucleotide
-    return seq
+    nucleotides = np.array(['A', 'G', 'C', 'T'])
+    seq = np.random.choice(nucleotides, size=length, p=distribution)
+    return ''.join(seq)
+
 
 def alpha(new_distribution, Q, i, k):
     """
     Returns the parameter alpha of the Metropolis - Hastings algorithm
     """
-    return min(1, (new_distribution[k]*Q[k,i])/(new_distribution[i]*Q[i,k]))
+    ratio = new_distribution[k] * Q[k, i] / (new_distribution[i] * Q[i, k])
+    return min(1, ratio)
 
-def get_M2(new_distribution,d2, l):
+def get_M2(new_distribution,d2, l, dir_constant):
     """
     Metropolis - Hastings implementation to get M2
     """
     P = np.zeros((4,4))
     iter = True
+
     while iter:
+
         # Random Markov matrix generation
         Q = np.zeros((4,4))
         i=0
         while i<4:
             dir = np.ones(4)
-            dir[i] = (12.5*np.exp(-l/4))/(np.sqrt(l/4))
+            dir[i] = dir_constant
             R = np.random.dirichlet(dir)
             if R[i] > 0.3:
                 Q[i,:] = R
                 i = i + 1
+
         # Time reversible matrix generation
         for i in range(4):
             for j in range(4):
                 if i == j:
-                    sum = np.sum((Q[i,k] * (1 - alpha(new_distribution,Q,i,k))) for k in range(4) if k != i)
+                    sum = 0
+                    for k in range(4):
+                        if k != i:
+                            sum += (Q[i,k] * (1 - alpha(new_distribution,Q,i,k)))
                     P[i,j] = Q[i,i] + sum
                 else:
                     P[i,j] = Q[i,j]*alpha(new_distribution,Q,i,j)
-        assert (np.abs(np.sum(new_distribution - np.matmul(
-                new_distribution,P)))) < 10**-6
+
+        assert (np.abs(np.sum(new_distribution - np.matmul(new_distribution,P)))) < 10**-6
+        
         # Adjust the matrix diagonalising (ensure matrix with determinant d2)
         vaps, _ = np.linalg.eig(P)
         vaps = sorted(vaps, reverse=True)
         A = symbols('A')
         eq = Eq(-d2+(((1-A)*vaps[1]+A)*((1-A)*vaps[2]+A)*((1-A)*vaps[3]+A)),0)
         sol = solve(eq, A)
+        
         # We only want the real solution between 0 and 1
-        res = 0
         for s in sol:
-            if s.is_real and s > 0 and s < 1:
-                res = s
-                res = np.float64(res)
-                P = (1-res)*P + res*np.identity(4)
+            if s.is_real and 0 < s < 1:
+                a = np.float64(s)
+                M2 = (1-a)*P + a*np.identity(4)
                 iter = False
                 break
-            elif s.is_complex:
-                b = np.imag(s)
-                a = sp.re(s)
-                if np.abs(b) < 10**-20 and a > 0 and a < 1:
-                    res = sp.re(s)
-                    res = np.float64(res)
-                    P = (1-res)*P + res*np.identity(4)
+            elif s.is_complex: #If imaginary part is negligible
+                if np.abs(np.imag(s)) < 10**-20 and 0 < sp.re(s) < 1:
+                    a = np.float64(sp.re(s))
+                    M2 = (1-a)*P + a*np.identity(4)
                     iter = False
                     break
-    return P
+    return M2
+
+def find_k(distribution, l, sq_det_D, exp_minus_l):
+    """
+    Finds a suitable value of k to satisfy the condition
+    detM1 > np.exp(-l)*np.sqrt(np.linalg.det(D_))/np.sqrt(np.linalg.det(D)).
+    """
+    epsilon = 1e-3  # Desired precision
+    lower_bound = 1
+    upper_bound = 100  # Adjust upper bound as needed
+    
+    while upper_bound - lower_bound > epsilon:
+        M1 = np.zeros((4,4))
+        mid = (lower_bound + upper_bound) / 2
+        dir_constant_mid = (mid*np.exp(-l/4))/(np.sqrt(l/4))
+        i = 0
+        while i<4:
+            dir = np.ones(4)
+            dir[i] = dir_constant_mid
+            R = np.random.dirichlet(dir)
+            if R[i] > 0.3:
+                M1[i,:] = R
+                i = i + 1
+        
+        new_distribution = np.matmul(distribution, M1)
+        D_ = np.diag(new_distribution)
+        detM1 = np.linalg.det(M1)
+        sq_det_D_ = np.sqrt(np.linalg.det(D_))
+        res = exp_minus_l * sq_det_D_ / sq_det_D
+
+        if detM1 > res:
+            upper_bound = mid
+        else:
+            lower_bound = mid
+
+    return (lower_bound + upper_bound) / 2
 
 def generate_random_matrix(distribution, l):
     """
     Returns the transition matrix M=M1M2 given a branch length
     and the distribution at the ancestor node.
     """
+
+    D = np.diag(distribution)
+    sq_det_D = np.sqrt(np.linalg.det(D))
+    exp_minus_l = np.exp(-l)
+    k = find_k(distribution, l, sq_det_D, exp_minus_l)
+    dir_constant = (k*np.exp(-l/4))/(np.sqrt(l/4))
+    
     res = 1
+
     # Compute M1
     while res >= 1:
         M1 = np.zeros((4,4))
         i=0
         while i<4:
             dir = np.ones(4)
-            dir[i] = (12.5*np.exp(-l/4))/(np.sqrt(l/4))
+            dir[i] = dir_constant
             R = np.random.dirichlet(dir)
             if R[i] > 0.3:
                 M1[i,:] = R
                 i = i + 1
 
         new_distribution = np.matmul(distribution,M1)
-        D = np.diag(distribution)
         D_ = np.diag(new_distribution)
-        res = np.exp(-l)*np.sqrt(np.linalg.det(D_))/np.sqrt(np.linalg.det(D))
+        sq_det_D_ = np.sqrt(np.linalg.det(D_))
+        res = exp_minus_l * sq_det_D_ / sq_det_D
         detM1 = np.linalg.det(M1)
-        #Checking conditions
-        if detM1 > np.exp(-l)*np.sqrt(np.linalg.det(D_))/np.sqrt(np.linalg.det(D)):
-            pass
-        else:
+
+        #If detM1 > res and res < 1, conditions are met. Otherwise, a new iteration.
+        if detM1 <= res:
             res = 1
 
-    d2 = np.exp(-l)*np.sqrt(np.linalg.det(D_))/(detM1*np.sqrt(np.linalg.det(D)))
-    # Obtain M2
-    M2 = get_M2(new_distribution,d2,l)
-
+    d2 = res * (1 / detM1)
+    M2 = get_M2(new_distribution,d2,l,dir_constant)
     detM2 = np.linalg.det(M2)
     assert(np.abs(detM2 - d2) < 10**-6)
+
     M = np.matmul(M1,M2)
     return M
 
--- utils/generate_sequences.py
+++ utils/generate_sequences.py
@@ -5,14 +5,7 @@ def generate_sequences(M, seq):
     Given the sequence seq of the ancestor node and the transition matrix M,
     returns the sequence of the descendant node
     """
-    new_seq = ""
-    for s in seq:
-        if s == "A":
-            new_seq += np.random.choice(['A', 'G', 'C', 'T'], p=M[0,:])
-        elif s == "G":
-            new_seq += np.random.choice(['A', 'G', 'C', 'T'], p=M[1,:])
-        elif s == "C":
-            new_seq += np.random.choice(['A', 'G', 'C', 'T'], p=M[2,:])
-        else:
-            new_seq += np.random.choice(['A', 'G', 'C', 'T'], p=M[3,:])
+    nucleotide_map = {'A': 0, 'G': 1, 'C': 2, 'T': 3}
+    row_indices = np.array([nucleotide_map[s] for s in seq])
+    new_seq = ''.join(np.random.choice(['A', 'G', 'C', 'T'], p=M[row_indices[i], :]) for i in range(len(seq)))
     return new_seq
